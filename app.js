bc8cff017a2f8e3bf8b9c5869c294e6b45e65da1
Sergio Gonzalez <sgonzalezlopez@gmail.com>
2022-12-29 15:34:32 +0100

    .

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Carga de valores de entorno locales.
 require('dotenv').config({ override: true })
 const path = require('path');
+if (typeof __corePath === 'undefined') global.__corePath = path.join(__dirname, './core')
+if (typeof __modulesPath === 'undefined') global.__modulesPath = path.join(__dirname, './node_modules')
+if (typeof __modelsPath === 'undefined') global.__modelsPath = []
+__modelsPath.push(path.join(__dirname, "./models"))
 
 const core = require(path.join(__corePath, '/app'))
 const express = require("express");
